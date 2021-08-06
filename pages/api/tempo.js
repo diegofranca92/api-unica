e5428cc9b2163007cd9e72e0c8d62d890d1cd5be
Diego França <diego.franca92@gmail.com>
2021-08-06 10:55:05 -0300

    api key

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,16 @@
-function tempo(request, response) {
+async function tempo(request, response) {
   const dynamicDate = new Date()
+  const apiSecret = process.env.CONVERTKIT_API_SECRET
+
+  const subscribersResponse = await fetch(
+    `https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`
+  )
+  const subscribersResponseJson = await subscribersResponse.json()
+  const inscritos = subscribersResponseJson.total_subscribers
 
   response.json({
-    date: dynamicDate.toGMTString()
+    date: dynamicDate.toGMTString(),
+    inscritos: inscritos
   })
 }
 
--- pages/tempo.js
+++ pages/tempo.js
@@ -1,5 +1,27 @@
 function Tempo(props) {
-  return <div>Tempo</div>
+  console.log('> Passando pelo Frontend;')
+  const dynamicDate = new Date()
+  const dynamicDateString = dynamicDate.toGMTString()
+
+  return (
+    <div>
+      <div>{dynamicDateString} (dinâmico)</div>
+      <div>{props.staticDateString} (estático)</div>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  console.log('> Passando pelo getStaticProps();')
+  const staticDate = new Date()
+  const staticDateString = staticDate.toGMTString()
+
+  return {
+    props: {
+      staticDateString
+    },
+    revalidate: 1
+  }
 }
 
 export default Tempo
